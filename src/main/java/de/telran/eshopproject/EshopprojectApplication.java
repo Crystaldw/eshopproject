a35b619fa7abb1822bfdc67b5311f5f209f654a4
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-01-18 14:41:49 +0100

    first commit

---
+++ src/main/java/de/telran/eshopproject/EshopprojectApplication.java
@@ -0,0 +1,13 @@
+package de.telran.eshopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/telran/eshopproject/EshopprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.eshopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
